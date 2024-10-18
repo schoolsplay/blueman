250580f638747792bd3dfaab76d56af7636c3949
stas zytkiewicz <stas@childsplay.mobi>
2024-10-18 15:19:15 +0200

    more work on pulse dialog

--- make_btp_release.sh
+++ make_btp_release.sh
@@ -34,6 +34,8 @@ cp -rf .venv3.8/lib/python3.8/site-packages/cairo release/.venv3.8/lib/python3.8
 cp -rf .venv3.8/lib/python3.8/site-packages/gi release/.venv3.8/lib/python3.8/site-packages
 cp -rf .venv3.8/lib/python3.8/site-packages/pygtkcompat release/.venv3.8/lib/python3.8/site-packages/
 cp -f ~/Github/SchoolsplayRepos/blueman/module/.libs/_blueman.so release/.venv3.8/lib/python3.8/site-packages/
+# needed when we want to run locally
+cp -f ~/Github/SchoolsplayRepos/blueman/module/.libs/_blueman.so .venv3.8/lib/python3.8/site-packages/
 # we need to move the ui and icons to the share folder
 mkdir -p release/.venv3.8/share/blueman
 # we symlink the ui and icons to the share folder
